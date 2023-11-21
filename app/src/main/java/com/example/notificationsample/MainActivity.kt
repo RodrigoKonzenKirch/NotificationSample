b28b8a037e4fa3db9a7c04e6097be6f581e086fa
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2023-11-21 16:43:06 -0300

    Added different types of notifications

--- app/src/main/java/com/example/notificationsample/MainActivity.kt
+++ app/src/main/java/com/example/notificationsample/MainActivity.kt
@@ -1,42 +1,50 @@
 package com.example.notificationsample
 
 import android.Manifest
-import android.os.Build
-import android.os.Bundle
-import android.app.NotificationManager
 import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.graphics.drawable.toBitmap
 import com.example.notificationsample.ui.theme.NotificationSampleTheme
 
 class MainActivity : ComponentActivity() {
-    private val CHANNEL_ID = "defaultChannelID"
-    private val NOTIFICATION_ID = 0
 
-
-    private val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(R.drawable.ic_default_notification)
-        .setContentTitle("This is an important notification")
-        .setContentText("Hi. You shouldn't miss this notification. It's very important. I would never bother you without a good reason!")
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+    val CHANNEL_ID = "defaultChannelID"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val bitmapImage = resources.getDrawable(R.drawable.abstract_wavy_green, applicationContext.theme).toBitmap()
+
         setContent {
             NotificationSampleTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    AppScreen("Notification is sent when app goes to the background")
+                    AppScreen(
+                        "Try out sending different types of notifications:",
+                        baseContext,
+                        CHANNEL_ID,
+                        bitmapImage
+                    )
                 }
             }
         }
@@ -45,20 +53,6 @@ class MainActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
         createNotificationChannel()
-    }
-
-    override fun onPause() {
-        val context = this
-        with(NotificationManagerCompat.from(this)) {
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                notify(NOTIFICATION_ID, builder.build())
-            }
-        }
-        super.onPause()
     }
 
     private fun createNotificationChannel() {
@@ -77,9 +71,95 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AppScreen(message: String, modifier: Modifier = Modifier) {
-    Text(
+fun AppScreen(
+    message: String,
+    context: Context,
+    channelId: String,
+    notificationImageBitmap: Bitmap,
+    modifier: Modifier = Modifier
+) {
+
+    val notificationIdBasic = 0
+    val notificationIdImage = 1
+    val notificationIdExpandable = 2
+    val defaultPadding = 12.dp
+
+    val basicNotificationBuilder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.ic_default_notification)
+        .setContentTitle("Basic notification")
+        .setContentText("Hi. You shouldn't miss this notification. It demonstrate a basic notification with title and text only!")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    val largeImageNotificationBuilder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.ic_default_notification)
+        .setContentTitle("Notification with image")
+        .setContentText("This is a notification that shows text and an image as a thumbnail")
+        .setLargeIcon(notificationImageBitmap)
+
+    val largeExpandableImageNotificationBuilder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.ic_default_notification)
+        .setContentTitle("Notification with image")
+        .setContentText("This is a notification that shows text and an image as a thumbnail. When the notification is expanded, the image maximizes it's size")
+        .setLargeIcon(notificationImageBitmap)
+        .setStyle(NotificationCompat.BigPictureStyle()
+            .bigPicture(notificationImageBitmap)
+            .bigLargeIcon(null))
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
             text = message,
-            modifier = modifier
-    )
+            modifier = modifier.padding(defaultPadding)
+        )
+
+        Button(
+            modifier = modifier.padding(defaultPadding),
+            onClick = {
+                with(NotificationManagerCompat.from(context)) {
+                    if (ActivityCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.POST_NOTIFICATIONS
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        notify(notificationIdBasic, basicNotificationBuilder.build())
+                    }
+                }
+
+            }) {
+            Text(text = "Send a basic notification")
+        }
+
+        Button(
+            modifier = modifier.padding(defaultPadding),
+            onClick = {
+                with(NotificationManagerCompat.from(context)) {
+                    if (ActivityCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.POST_NOTIFICATIONS
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        notify(notificationIdImage, largeImageNotificationBuilder.build())
+                    }
+                }
+
+            }) {
+            Text(text = "Send image notification")
+        }
+
+        Button(
+            modifier = modifier.padding(defaultPadding),
+            onClick = {
+                with(NotificationManagerCompat.from(context)) {
+                    if (ActivityCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.POST_NOTIFICATIONS
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        notify(notificationIdExpandable, largeExpandableImageNotificationBuilder.build())
+                    }
+                }
+
+            }) {
+            Text(text = "Send expandable image notification")
+        }
+    }
 }
